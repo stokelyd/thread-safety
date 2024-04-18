8a7aa762e7ee362d36ed3fbb600110c2580f9812
Stokely Diamantis <stokely_diamantis@sfu.ca>
2024-04-18 02:35:33 -0700

    migrate project template

---
+++ se-fault-tolerant-template/test/c/free-wild.c
@@ -0,0 +1,10 @@
+
+#include<inttypes.h>
+#include<stdlib.h>
+
+uintptr_t
+tryFree(uint32_t chooseValid) {
+  uint8_t* heap = malloc(5);
+  free(chooseValid ? heap : (uint8_t)0xAbadCAB);
+  return (uintptr_t)heap;
+}
---
+++ se-fault-tolerant-template/tools/tolerator/config.h.cmake
@@ -0,0 +1,6 @@
+
+#pragma once
+
+#define RUNTIME_LIB "tolerator-rt"
+#cmakedefine CMAKE_ARCHIVE_OUTPUT_DIRECTORY "@CMAKE_ARCHIVE_OUTPUT_DIRECTORY@"
+
